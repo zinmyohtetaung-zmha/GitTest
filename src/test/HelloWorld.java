64a38ca19de04cfbd89ca4bb8b32678a16480bab
zinmyohtetaung-zmha <74319335+zinmyohtetaung-zmha@users.noreply.github.com>
2023-02-05 16:23:19 +0630

    Update HelloWorld.java

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 		System.out.println("Hello Git");
 		System.out.println("text vi");
 		System.out.println("Inside Develper_001 Branch");
+		System.out.println("I changed from remote master.");
 	}
 
 }
